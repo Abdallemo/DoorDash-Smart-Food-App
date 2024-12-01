4c201d12d866aa86343e29c403e198f674e32625
EngCamo <learn3038it@gmail.com>
2024-12-02 01:38:57 +0800

    testing Api

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:dashdoor/components/my_drawer.dart';
+import 'package:dashdoor/pages/my_current_location.dart';
+import 'package:dashdoor/pages/my_sliver_app_bar.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -7,8 +9,31 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Door Dash",)),
+      // backgroundColor: Theme.of(context).colorScheme.background,
       drawer: MyDrawer(),
+      body: NestedScrollView(
+        headerSliverBuilder: (context, innerBoxIsScrolled) => [
+          MySliverAppBar(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Divider(
+                    indent: 25,
+                    endIndent: 25,
+                    color: Theme.of(context).colorScheme.secondary,
+                  ),
+
+                  //location
+                  MyCurrentLocation(),
+                  //description box
+                ],
+              ),
+              title: Text("title"))
+        ],
+        body: Container(
+          color: Colors.amber.shade600,
+        ),
+      ),
     );
   }
 }
